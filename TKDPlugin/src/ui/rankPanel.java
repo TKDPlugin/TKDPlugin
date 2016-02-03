8df0954647f3882bbd161f3579991de70d87ce9d
njushishuo <675342907@qq.com>
2016-02-03 15:11:57 +0800

    去除不必要的导入

--- TKDPlugin/src/ui/rankPanel.java
+++ TKDPlugin/src/ui/rankPanel.java
@@ -1,8 +1,6 @@
 package ui;
 
-import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
