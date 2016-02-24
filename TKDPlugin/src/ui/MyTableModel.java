4ed3b7c4d9e5e8d7a7ee4f3da67fc07b42933170
njushishuo <675342907@qq.com>
2016-02-24 20:55:57 +0800

    巴巴

--- TKDPlugin/src/ui/MyTableModel.java
+++ TKDPlugin/src/ui/MyTableModel.java
@@ -38,8 +38,11 @@ public class MyTableModel extends AbstractTableModel {
 			  title.add("姓名");
 			  title.add("队伍");
 <<<<<<< HEAD
+<<<<<<< HEAD
 			  title.add("级别");
 			  title.add("选择");
+=======
+>>>>>>> parent of 165717e... 根据李老师新的需求修改
 =======
 >>>>>>> parent of 165717e... 根据李老师新的需求修改
 			  this.setData(athleteList);
@@ -90,8 +93,11 @@ public class MyTableModel extends AbstractTableModel {
 			temp.add( athleteList.get(i).getName());
 			temp.add( athleteList.get(i).getTeam());
 <<<<<<< HEAD
+<<<<<<< HEAD
 			temp.add(athleteList.get(i).getLevelName());
 			temp.add(new JRadioButton("",false));
+=======
+>>>>>>> parent of 165717e... 根据李老师新的需求修改
 =======
 >>>>>>> parent of 165717e... 根据李老师新的需求修改
 			data.add(temp);
